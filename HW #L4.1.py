01e95d5cf1bfb70bf994aa809f4e64ef6cfbcd6d
Ilusha <4ylkov@mail.ru>
2018-04-13 11:54:17 +0300

    initial

---
+++ HW #L4.1.py
@@ -0,0 +1,14 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+def top_student(list_student):
+    pass
+
+
+list_student = [{name: 'Alexey', rate: 2, course: 'Python'}, 
+                {name: 'Anton', rate: 4, course: 'Python'}, 
+                {name: 'Nasty', rate: 5, course: 'English'}, 
+                {name: 'Marina', rate: 2, course: 'English'}]
+
